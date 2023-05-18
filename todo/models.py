3b948e1f156b97dfbe33e589fa63c79743d178b9
kaleab Teweldbrhan Abay <42609047+kaleabteweld@users.noreply.github.com>
2023-05-18 12:13:14 +0300

    init

---
+++ todo/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return '<User: {}>'.format(self.name)
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '<Todo: {}>'.format(self.title)
